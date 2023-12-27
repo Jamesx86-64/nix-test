d525ca1d247723334e6a840fd5d2f0534b0b1a2e
James <76922491+Jamesx86-64@users.noreply.github.com>
2023-12-27 22:56:20 +0000

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,10 @@
+{
+  description = "Unofficial Acer Gaming RGB keyboard backlight and Turbo mode Linux kernel module (Acer Predator, Acer Helios, Acer Nitro)";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+  };
+}
